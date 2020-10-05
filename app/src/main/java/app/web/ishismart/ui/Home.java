2ef4b8ad7bcd6b1e8a9dd08ec7767b1a5ceced43
Ngara K <ngarakiringo@gmail.com>
2020-10-05 19:28:58 +0300

    getting morning tea articles

--- app/src/main/java/app/web/ishismart/ui/Home.java
+++ app/src/main/java/app/web/ishismart/ui/Home.java
@@ -1,21 +1,57 @@
 package app.web.ishismart.ui;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 
-import app.web.ishismart.R;
+import java.util.ArrayList;
+import java.util.List;
+
+import app.web.ishismart.adapters.MorningTeaRecyclerAdapter;
 import app.web.ishismart.databinding.ActivityHomeBinding;
+import app.web.ishismart.models.MorningTea;
+
+import static app.web.ishismart.utils.AppUtils.morningTeaReference;
 
 public class Home extends AppCompatActivity {
 
     private static String TAG = "Anonymous Account Activity";
     private ActivityHomeBinding binding;
+    private MorningTeaRecyclerAdapter teaRecyclerAdapter;
+    private List<MorningTea> morningTeaList = new ArrayList<>();
+    private MorningTea morningTea;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        binding  = ActivityHomeBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        getMorningTea();
+    }
+
+    private void getMorningTea() {
+        morningTeaReference.orderBy("post_date.timestamp", Query.Direction.DESCENDING)
+            .get().addOnSuccessListener(querySnapshot -> {
+
+                for (QueryDocumentSnapshot queryDocumentSnapshot : querySnapshot) {
+                    Log.d(TAG, "MORNING TEA: " + queryDocumentSnapshot.getId());
+
+                    /*converting snapshot to pojo class*/
+                    morningTea = queryDocumentSnapshot.toObject(MorningTea.class);
+                    morningTeaList.add(morningTea);
+                }
+
+                /*setting adapter*/
+                teaRecyclerAdapter = new MorningTeaRecyclerAdapter(morningTeaList);
+                binding.morningTeaRecyclerview.setAdapter(teaRecyclerAdapter);
+            }).addOnFailureListener(e -> Log.d(TAG, "onFailure() returned: " + e.getMessage()));
     }
 
     @Override
--- app/src/main/java/app/web/ishismart/utils/AppUtils.java
+++ app/src/main/java/app/web/ishismart/utils/AppUtils.java
@@ -2,9 +2,15 @@ package app.web.ishismart.utils;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 
 public class AppUtils {
     public static FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
     public static FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+    /*morning tea collection reference*/
+    public static CollectionReference morningTeaReference = FirebaseFirestore.getInstance()
+            .collection("posts").document("morning_tea").collection("tea_collection");
 }
